9ef614fc3e87158f47a1caa2bd0680a42934d1cf
Believe Liphadzi <122865634+pbliphadzi@users.noreply.github.com>
2023-12-06 18:32:17 +0000

    again

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
-# Create file in /tmp
-
-file { '/tmp/school' :
-    path    => '/tmp/school',
-    mode    => '0744',
-    owner   => 'www-data',
-    group   => 'www-data',
-    content => 'I love Puppet',
-    }
+# create a file with permissions and a content
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet', # This is another comment
+}
